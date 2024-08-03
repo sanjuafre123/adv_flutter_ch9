ae084f24b11f292dc0e676d46754253d16388df2
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2024-08-03 13:30:00 +0530

    Update Cart_Screen.dart

--- lib/Lec-9.1.2/Screens/cart_screen/Cart_Screen.dart
+++ lib/Lec-9.1.2/Screens/cart_screen/Cart_Screen.dart
@@ -14,47 +14,62 @@ class CartScreen extends StatelessWidget {
     BeautyProvider beautyProviderTrue =
         Provider.of<BeautyProvider>(context, listen: true);
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Cart'),
-        ),
-        body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                ...List.generate(
-                  beautyProviderTrue.cart.length,
-                  (index) => Row(
-                    children: [
-                      Container(
-                        height: 100,
-                        width: 100,
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                          image: NetworkImage(beautyProviderTrue
-                              .cart[index]['images'][0]),
-                        ),
-
-                        ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Cart'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            ...List.generate(
+              beautyProviderTrue.cart.length,
+              (index) => Row(
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            beautyProviderTrue.cart[index]['images'][0]),
                       ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
                         children: [
-                          Text(beautyProviderTrue.beautyModal!.product[selectedIndex].title,style: TextStyle(fontSize: 18),),
-                          Row(
-                            children: [
-                              Text('Rs. ${beautyProviderTrue.price}'.toString()),
-                            ],
+                          Text(
+                            beautyProviderTrue
+                                .beautyModal!.product[selectedIndex].title,
+                            style: TextStyle(fontSize: 15),
                           ),
                         ],
                       ),
 
-
+                      Row(
+                        children: [
+                          Text(
+                            'Rs. ${beautyProviderTrue.beautyModal!.product[selectedIndex].price}'
+                                .toString(),
+                          ),
+                        ],
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          beautyProviderTrue.deleteP(index);
+                        },child: Icon(Icons.delete)),
                     ],
                   ),
-                )
-              ],
-            )));
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
 
